d85596a65b4aa7643e9ee077eee9cd9a88e8b2ef
Aditya Lahane <adityalahane31@gmail.com>
2023-05-24 04:03:13 +0530

    Animated Home Page Text

--- assets/js/main.js
+++ assets/js/main.js
@@ -305,3 +305,20 @@
    */
   new PureCounter();
 })();
+
+// Home Page Text Animation 
+const spans = document.querySelectorAll('.word h1');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
