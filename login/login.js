d8cd4f1a0b9cf48032299fa582d9e07329697e00
Vaishnavi-Patil2211 <Vaishnavi-Patil2211@users.noreply.github.com>
2023-07-18 09:25:35 +0000

    Prettified Code!

--- login/login.js
+++ login/login.js
@@ -1,19 +1,18 @@
-        let signupBtn= document.getElementById("signupBtn");
-        let loginBtn= document.getElementById("loginBtn");
-        let nameField= document.getElementById("nameField");
-        let title = document.getElementById("title");
+let signupBtn = document.getElementById("signupBtn");
+let loginBtn = document.getElementById("loginBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
 
-        loginBtn.onclick = function(){
-            nameField.style.maxHeight="0";
-            title.innerHTML ="Login";
-            signupBtn.classList.add("disable");
-            loginBtn.classList.remove("disable");
-        }
+loginBtn.onclick = function () {
+  nameField.style.maxHeight = "0";
+  title.innerHTML = "Login";
+  signupBtn.classList.add("disable");
+  loginBtn.classList.remove("disable");
+};
 
-        signupBtn.onclick = function(){
-            nameField.style.maxHeight="60px";
-            title.innerHTML ="Sign Up";
-            signupBtn.classList.remove("disable");
-            loginBtn.classList.add("disable");
-        }
-        
+signupBtn.onclick = function () {
+  nameField.style.maxHeight = "60px";
+  title.innerHTML = "Sign Up";
+  signupBtn.classList.remove("disable");
+  loginBtn.classList.add("disable");
+};
