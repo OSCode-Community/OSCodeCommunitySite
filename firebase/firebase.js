1a160337f18a50c19ed80e904443a4b50f62eb44
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-07-19 13:12:55 +0530

    Added firebase authentication to the website

---
+++ firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+export { database, auth };
--- login/login.js
+++ login/login.js
@@ -1,18 +1,68 @@
-let signupBtn = document.getElementById("signupBtn");
-let loginBtn = document.getElementById("loginBtn");
-let nameField = document.getElementById("nameField");
-let title = document.getElementById("title");
+import { database, auth } from "../firebase/firebase.js";
+import {
+  update,
+  ref,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-loginBtn.onclick = function () {
-  nameField.style.maxHeight = "0";
-  title.innerHTML = "Login";
-  signupBtn.classList.add("disable");
-  loginBtn.classList.remove("disable");
-};
+// Login with email and password
+login.addEventListener("click", (e) => {
+  e.preventDefault();
 
-signupBtn.onclick = function () {
-  nameField.style.maxHeight = "60px";
-  title.innerHTML = "Sign Up";
-  signupBtn.classList.remove("disable");
-  loginBtn.classList.add("disable");
-};
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      const date = new Date();
+
+      // Update database and redirect to the homepage
+      update(ref(database, "users/" + user.uid), {
+        last_login: date,
+      }).then(() => (window.location.href = "/"));
+    })
+    .catch((error) => {
+      // Handle errors here
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      document.getElementById("error-msg").innerHTML = errorMessage;
+    });
+});
+
+// Login with google
+const provider = new GoogleAuthProvider();
+
+googleLogin.addEventListener("click", (e) => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+
+      // Redirecting to the home page
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      document.getElementById("error-msg").innerHTML = errorMessage;
+    });
+});
+
+togglePassword.addEventListener("click", (e) => {
+  if (document.getElementById("password").type == "password") {
+    document.getElementById("password").type = "text";
+  } else {
+    document.getElementById("password").type = "password";
+  }
+});
